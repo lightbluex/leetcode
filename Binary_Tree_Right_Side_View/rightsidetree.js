20014a94785c21b07b12185e967ce760eb9a9ebf
Xia, Zewei | Eric | TRVDD <zewei.xia@mail.rakuten.com>
2015-10-30 15:22:54 +0900

    Add file

---
+++ Binary_Tree_Right_Side_View/rightsidetree.js
@@ -0,0 +1,28 @@
+/**
+ * https://leetcode.com/problems/binary-tree-right-side-view/
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    resultList=[];
+    markTree(root, 0);
+    return resultList;
+};
+
+
+var resultList = [];
+var markTree = function(node, deep){
+  if(node) {
+    resultList[deep]= node.val;
+    deep++;
+    if(node.left, deep) markTree(node.left, deep);
+    if(node.right, deep) markTree(node.right, deep);
+  }
+};
