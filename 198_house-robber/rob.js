c47698954a7800586facf2e096a6e5dd6e84684b
Xia, Zewei | Eric | TRVDD <zewei.xia@mail.rakuten.com>
2015-11-25 18:16:55 +0900

    add initializing

--- 198_house-robber/rob.js
+++ 198_house-robber/rob.js
@@ -4,6 +4,10 @@
  * @return {number}
  */
 var rob = function(nums) {
+  for (var i = 0; i < nums.length; i++) {
+    max[i]=0;
+  }
+  maxSum=0;
   next(0, nums, 0, 0);
   next(0, nums, 1, 0);
   return maxSum;
@@ -15,8 +19,8 @@ var maxSum=0;
 var next = function(sum, num, current, skip) {
   sum += num[current+skip];
   current += skip;
-  if(sum>maxSum) maxSum = sum;
-  if(max[current]&&sum<max[current]) return false;
+  if(sum>=maxSum) maxSum = sum;
+  if(sum<=max[current]) return false;
   max[current]=sum;
   if(current+2<num.length) next(sum, num, current, 2);
   if(current+3<num.length) next(sum, num, current, 3);
