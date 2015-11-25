2cda61786933927f2e97790deef49f91f59d3537
Xia, Zewei | Eric | TRVDD <zewei.xia@mail.rakuten.com>
2015-11-25 18:00:08 +0900

    fix

--- 198_house-robber/rob.js
+++ 198_house-robber/rob.js
@@ -6,15 +6,19 @@
 var rob = function(nums) {
   next(0, nums, 0, 0);
   next(0, nums, 1, 0);
-  return max;
+  return maxSum;
 };
 
-var max=0;
+var max=[];
+var maxSum=0;
 
 var next = function(sum, num, current, skip) {
   sum += num[current+skip];
   current += skip;
-  if(sum>max) max = sum;
+  if(sum>maxSum) maxSum = sum;
+  if(max[current]&&sum<max[current]) return false;
+  max[current]=sum;
   if(current+2<num.length) next(sum, num, current, 2);
   if(current+3<num.length) next(sum, num, current, 3);
+  return true;
 };
