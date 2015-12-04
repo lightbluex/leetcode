3cd6c72069a22d9e7b696c34cc496d5995750dcf
Xia, Zewei | Eric | TRVDD <zewei.xia@mail.rakuten.com>
2015-12-04 18:01:37 +0900

    shortestPalindrome

--- 214_shortest-palindrome/shortestPalindrome.js
+++ 214_shortest-palindrome/shortestPalindrome.js
@@ -4,5 +4,22 @@
  * @return {string}
  */
 var shortestPalindrome = function(s) {
+  var r = reverse(s);
+  console.log(r);
+  for (var i = 0; i <= s.length; i++) {
+    commonR = r.slice(i);
+    commonS = s.slice(0,s.length-i);
+    if(commonR == commonS) break;
+  }
+  console.log(i);
+  result = [r.slice(0,i), s].join("");
+  return result;
+};
 
+var reverse = function(s){
+  var r=[];
+  for (var i = 0; i < s.length; i++) {
+    r[i] = s[s.length-i-1];
+  }
+  return r.join("");
 };
